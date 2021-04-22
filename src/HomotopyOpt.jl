57868bfc9dea81760443da1db3d8f441dc87e405
Matthias Himmelmann <65544132+matthiashimmelmann@users.noreply.github.com>
2021-04-22 16:46:36 +0200

    Update HomotopyOpt.jl

--- src/HomotopyOpt.jl
+++ src/HomotopyOpt.jl
@@ -309,7 +309,7 @@ function watch(result::OptimizationResult; totalseconds=5.0)
         pointsys=[GLMakie.Point3f0(p) for p in ps]
         GLMakie.record(initplt, "watch$startingtime.gif", 1:length(pointsys); framerate = Int64(round(framespersecond))) do i
             GLMakie.scatter!(initplt, pointsys[i];
-                            color=:black, markersize=4.0)
+                            color=:black, markersize=20.0)
         end
         return(initplt)
     end
